22f934fca3a3fec427ab4ffe7bbcd97a280e379c
bhumika24 <bhumika.sudhakar@gmail.com>
2022-11-01 11:31:09 +0000

    Locking Remote State

--- backend.tf
+++ backend.tf
@@ -3,5 +3,6 @@ backend "s3" {
   bucket         = "clickittech-terraform-best-practices-1"
   key            = "terraform.tfstate"
   region         = "us-west-1"
+  dynamodb_table = "terraform_locks"
 }
 }
